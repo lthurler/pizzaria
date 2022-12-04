41eab10fb546184a5e9c842c55734a488935ed17
lthurler <leothurler@gmail.com>
2022-12-04 01:16:36 -0300

    ajuste footer respons

--- js/script.js
+++ js/script.js
@@ -1,12 +1,50 @@
-let show = true;
-const menuContent = document.querySelector('.content');
-const menuToggle = menuContent.querySelector('.menu-toggle');
+// let show = true;
+// const menuContent = document.querySelector('.content');
+// const menuToggle = menuContent.querySelector('.menu-toggle');
 
 
-menuToggle.addEventListener('click', () => {
+// menuToggle.addEventListener('click', () => {
 
-    document.body.style.overflow = show ? 'hidden' : 'initial'
+//     document.body.style.overflow = show ? 'hidden' : 'initial'
 
-    menuContent.classList.toggle('on', show);
-    show = !show;
-})
+//     menuContent.classList.toggle('on', show);
+//     show = !show;
+// })
+
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+
+function smoothScroll(e) {
+    e.preventDefault();
+  
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+  
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  
+    setTimeout(() => {
+      if (menu.classList.contains("menu-active")) {
+        menu.classList.remove("menu-active");
+      }
+    }, 500);
+}
+
+
+[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      menu.classList.toggle("menu-active");
+    });
+});
+  
+  allLinks.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
+    menu.classList.remove("menu-active");
+});
